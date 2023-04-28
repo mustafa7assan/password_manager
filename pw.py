e8ddb7ea50118a7794026e9c8293073ae75ba6da
Mustafa Hassan <mustafahassan.dev@gmail.com>
2023-04-28 22:14:53 +0200

    First commit

---
+++ pw.py
@@ -0,0 +1,58 @@
+import sys
+import pyperclip
+
+passwords = {}
+
+
+def load_passwords():
+    password_file = open('passwords.txt')
+    lines = password_file.readlines()
+    for line in lines:
+        account, password = line.strip().split(',')
+        passwords[account] = password
+    password_file.close()
+
+
+def add_password(account, password):
+    password_file = open('passwords.txt', 'a')
+    password_file.write(f'{account},{password}')
+    password_file.close()
+
+
+load_passwords()
+
+
+def add_account():
+    account = input('Account name: ')
+    password = input('Account password : ')
+    passwords[account] = password
+    add_password(account, password)
+    print('New account is added.')
+
+
+if len(sys.argv) < 2:
+    print('Usage: python3 pw.py [account] - copy account password ')
+    sys.exit()
+
+
+account = sys.argv[1]
+
+if account in passwords:
+    pyperclip.copy(passwords[account])
+    print(f'{account} password is copied to clipboard')
+else:
+    print(f'There no account named {account}')
+    add = input('Do want to add to it password manager [Y/N]: ')
+    if add.lower() == 'y' or add.lower() == 'yes':
+        add_account()
+
+
+'''
+echo "# password_manager" >> README.md
+git init
+git add README.md
+git commit -m "first commit"
+git branch -M main
+git remote add origin git@github.com:mustafa7assan/password_manager.git
+git push -u origin main
+'''
